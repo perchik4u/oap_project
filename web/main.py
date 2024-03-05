79b145450224c5e58c1dcc3439b2000c48db56fb
blessix <rangemihred2@gmail.com>
2024-03-05 23:09:25 +1000

    work with fastapi

--- web/main.py
+++ web/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return FileResponse("web/index.html")
